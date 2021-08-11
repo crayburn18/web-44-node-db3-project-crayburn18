07f2151de7934ed30b596b510e58534ef36b3d92
Connor Rayburn <connorrayburn.work@gmail.com>
2021-08-11 16:16:03 -0700

    mvp

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -115,7 +115,7 @@ async function findById(scheme_id) { // EXERCISE B
     
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -136,12 +136,24 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+ const rows =  await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('step_number')
+
+      if(!rows[0].step_id) return []
+      return rows
 }
 
 function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+ return db('schemes').insert(scheme)
+  .then(([scheme_id]) => {
+    return db('schemes').where('scheme_id', scheme_id).first()
+  })
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
@@ -150,6 +162,17 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+ return db('steps').insert({
+   ...step,
+   scheme_id
+ })
+  .then(() => {
+    return db('steps as st')
+    .leftJoin('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+    .select('step_id', 'step_number', 'instructions', 'scheme_name')
+    .orderBy('step_number')
+    .where('sc.scheme_id', scheme_id)
+  })
 }
 
 module.exports = {
